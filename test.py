420f3f0af98ef86a7ff136066f0541f5ab965560
Shawn Zhong <github@shawnzhong.com>
2020-12-06 08:59:37 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,106 +1,78 @@
 import os, sys, re, os.path
 import subprocess, datetime, time, signal
 
+CFG_STD = "config-std.h"
+CFG_CURR = "config-std.h"
+
 
 def replace(filename, pattern, replacement):
-	f = open(filename)
-	s = f.read()
-	f.close()
-	s = re.sub(pattern,replacement,s)
-	f = open(filename,'w')
-	f.write(s)
-	f.close()
+    f = open(filename)
+    s = f.read()
+    f.close()
+    s = re.sub(pattern, replacement, s)
+    f = open(filename, 'w')
+    f.write(s)
+    f.close()
 
-
-dbms_cfg = ["config-std.h", "config.h"]
-
-
-# test config: 
-algs = ['DL_DETECT', 'NO_WAIT', 'HEKATON', 'SILO', 'TICTOC']
-indices=['INDEX_BTREE','INDEX_HASH']
-num_threads_lst = [2 ** n for n in range(1, 8)]
-workloads = ["YCSB", "TPCC"]
-
-jobs = {
-
-}
-
-
-for workload in workloads:
-	for alg in algs:
-		for index in indices:
-			for num_threads in num_threads_lst:
-				jobs[f"{workload}_{alg}_{index}_{num_threads}"] = {
-					"WORKLOAD"			: workload,
-					"CORE_CNT"			: num_threads,
-					"CC_ALG"			: alg,
-					"INDEX_STRUCT"		: index
-				}
-
-print(jobs)
 
 def test_compile(job):
-	os.system("cp " + dbms_cfg[0] + ' '+ dbms_cfg[1])
-	for (param, value) in job.iteritems():
-		pattern = r"\#define\s" + re.escape(param) + r'.*'
-		replacement = "#define " + param + ' ' + str(value)
-		replace(dbms_cfg[1], pattern, replacement)
-	os.system("make clean > temp.out 2>&1")
-	ret = os.system("make -j8 > temp.out 2>&1")
-	if ret != 0:
-		print(f"ERROR in compiling job={job}")
-		exit(0)
-	print("PASS Compile\t\tworkload=%s,\t CORE_CNT=, \talg=%s, \tIndex=, " % (job['WORKLOAD'], job['CORE_CNT'], job['CC_ALG'], job['Index']))
+    os.system("cp " + CFG_STD + ' ' + CFG_CURR)
+    for (param, value) in job.iteritems():
+        pattern = r"\#define\s" + re.escape(param) + r'.*'
+        replacement = "#define " + param + ' ' + str(value)
+        replace(CFG_CURR, pattern, replacement)
+    ret = os.system("make -j > temp.out 2>&1")
+    if ret != 0:
+        print(f"ERROR in compiling job={job}")
+        exit(0)
+    print(f"PASS Compile{job}")
+    os.system('rm temp.out')
 
-# def test_run(test = '', job=None):
-# 	app_flags = ""
-# 	if test == 'read_write':
-# 		app_flags = "-Ar -t1"
-# 	if test == 'conflict':
-# 		app_flags = "-Ac -t4"
-#
-# 	#os.system("./rundb %s > temp.out 2>&1" % app_flags)
-# 	#cmd = "./rundb %s > temp.out 2>&1" % app_flags
-# 	cmd = "./rundb %s" % (app_flags)
-# 	start = datetime.datetime.now()
-# 	ww
-# 	process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-# 	timeout = 10 # in second
-# 	while process.poll() is None:
-# 		time.sleep(1)
-# 		now = datetime.datetime.now()
-# 		if (now - start).seconds > timeout:
-# 			os.kill(process.pid, signal.SIGKILL)
-# 			os.waitpid(-1, os.WNOHANG)
-# 			print("ERROR. Timeout cmd=%s" % cmd)
-# 			exit(0)
-# 	if "PASS" in process.stdout.read():
-# 		if test != '':
-# 			print("PASS execution. \talg=%s,\tworkload=%s(%s)" % \
-# 				(job["CC_ALG"], job["WORKLOAD"], test))
-# 		else :
-# 			print("PASS execution. \talg=%s,\tworkload=%s" % \
-# 				(job["CC_ALG"], job["WORKLOAD"]))
-# 		return
-# 	print("FAILED execution. cmd = %s" % cmd)
-# 	exit(0)
-#
-# # def run_all_test(jobs) :
-# # 	for (jobname, job) in jobs.iteritems():
-# # 	jobs = {}
-# #
-# # # run YCSB tests
-# # jobs = {}
-# # for alg in algs:
-# # 	insert_job(alg, 'YCSB')
-# # run_all_test(jobs)
-# #
-# # # run TPCC tests
-# # jobs = {}
-# # for alg in algs:
-# # 	insert_job(alg, 'TPCC')
-# # run_all_test(jobs)
-#
-# os.system('cp config-std.h config.h')
-# os.system('make clean > temp.out 2>&1')
-# os.system('rm temp.out')
+
+def test_run(job, app_flags=""):
+    cmd = f"./rundb {app_flags}"
+    start = datetime.datetime.now()
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    timeout = 10  # in second
+    while process.poll() is None:
+        time.sleep(1)
+        now = datetime.datetime.now()
+        if (now - start).seconds > timeout:
+            os.kill(process.pid, signal.SIGKILL)
+            os.waitpid(-1, os.WNOHANG)
+            print("ERROR. Timeout cmd=%s" % cmd)
+            exit(0)
+
+    if "PASS" not in process.stdout.readlines():
+        print("FAILED execution. cmd = {cmd}")
+        exit(0)
+
+    print(f"PASS execution. {job}")
+
+
+def main():
+    algs = ['DL_DETECT', 'NO_WAIT', 'HEKATON', 'SILO', 'TICTOC']
+    indices = ['INDEX_BTREE', 'INDEX_HASH']
+    num_threads_lst = [2 ** n for n in range(1, 8)]
+    workloads = ["YCSB", "TPCC"]
+
+    jobs = {
+        f"{workload}_{alg}_{index}_{num_threads}": {
+            "WORKLOAD": workload,
+            "CORE_CNT": num_threads,
+            "CC_ALG": alg,
+            "INDEX_STRUCT": index
+        }
+        for workload in workloads
+        for alg in algs
+        for index in indices
+        for num_threads in num_threads_lst
+    }
+
+    for job in jobs:
+        test_compile(job)
+        test_run(job)
+
+
+if __name__ == '__main__':
+    main()
